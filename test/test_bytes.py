bdbc37b535f21e27962bfaabce9d2fddedbf65c1
yakovb <y@kov.com>
2024-09-29 21:01:42 +1000

    failing test for lines

--- test/test_bytes.py
+++ test/test_bytes.py
@@ -1,9 +1,8 @@
 import pytest
 import subprocess
 
-def run_on(filename):
+def count_bytes_on(filename):
     res = subprocess.run(['wc', '--bytes', filename], capture_output=True, text=True)
-    print(res)
     expected = int(res.stdout.split()[0])
     
     ccwc = subprocess.run(['../src/ccwc/ccwc', '-c', filename], capture_output=True, text=True)
@@ -12,15 +11,31 @@ def run_on(filename):
     return (expected, actual)
 
 
+def count_lines_on(filename):
+    res = subprocess.run(['wc', '--lines', filename], capture_output=True, text=True)
+    expected = int(res.stdout.split()[0])
+    
+    ccwc = subprocess.run(['../src/ccwc/ccwc', '-l', filename], capture_output=True, text=True)
+    actual = int(ccwc.stdout.split()[0])
+
+    return (expected, actual)
+
+
 def test_zero():
-    expected, actual = run_on('zero.txt')
-    assert expected == 0, 'wc should return 0!'
+    expected, actual = count_bytes_on('zero.txt')
+    assert expected == 0, 'wc should return 0 bytes!'
     assert actual == expected, f'Should be {expected}, got {actual}'
 
 
 def test_bytes():
-    expected, actual = run_on('test.txt')
-    assert expected == 342190, 'wc should return 342190!'
+    expected, actual = count_bytes_on('test.txt')
+    assert expected == 342190, 'wc should return 342190 bytes!'
+    assert actual == expected, f'Should be {expected}, got {actual}'
+
+
+def test_lines():
+    expected, actual = count_lines_on('test.txt')
+    assert expected == 7145, 'wc should return 7145 lines!'
     assert actual == expected, f'Should be {expected}, got {actual}'
 
 
