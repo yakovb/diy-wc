97d6d8ba62122f67d3d3531a9b80dfad218ff50d
yakovb <y@kov.com>
2024-09-29 20:50:51 +1000

    count bytes, make zero test work

--- test/test_bytes.py
+++ test/test_bytes.py
@@ -1,14 +1,17 @@
-import subprocess
 import pytest
+import subprocess
 
 def test_zero():
     f = 'zero.txt'
     res = subprocess.run(['wc', '--bytes', f], capture_output=True, text=True)
-    wc = int(res.stdout.split()[0])
-    ccwc = 1
+    print(res)
+    expected = int(res.stdout.split()[0])
+    
+    ccwc = subprocess.run(['../src/ccwc/ccwc', '-c', f], capture_output=True, text=True)
+    actual = int(ccwc.stdout.split()[0])
 
-    assert wc == 0, 'wc should return 0!'
-    assert ccwc == wc, f'Should be zero, got {ccwc}'
+    assert expected == 0, 'wc should return 0!'
+    assert actual == expected, f'Should be zero, got {actual}'
 
 
 if __name__ == '__main__':
