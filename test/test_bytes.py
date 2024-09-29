55701b5cdb6489b21f107f1e824ca2af3448f01b
yakovb <y@kov.com>
2024-09-29 20:57:23 +1000

    test for bytes, and simplify tests

--- test/test_bytes.py
+++ test/test_bytes.py
@@ -1,17 +1,27 @@
 import pytest
 import subprocess
 
-def test_zero():
-    f = 'zero.txt'
-    res = subprocess.run(['wc', '--bytes', f], capture_output=True, text=True)
+def run_on(filename):
+    res = subprocess.run(['wc', '--bytes', filename], capture_output=True, text=True)
     print(res)
     expected = int(res.stdout.split()[0])
     
-    ccwc = subprocess.run(['../src/ccwc/ccwc', '-c', f], capture_output=True, text=True)
+    ccwc = subprocess.run(['../src/ccwc/ccwc', '-c', filename], capture_output=True, text=True)
     actual = int(ccwc.stdout.split()[0])
 
+    return (expected, actual)
+
+
+def test_zero():
+    expected, actual = run_on('zero.txt')
     assert expected == 0, 'wc should return 0!'
-    assert actual == expected, f'Should be zero, got {actual}'
+    assert actual == expected, f'Should be {expected}, got {actual}'
+
+
+def test_bytes():
+    expected, actual = run_on('test.txt')
+    assert expected == 342190, 'wc should return 342190!'
+    assert actual == expected, f'Should be {expected}, got {actual}'
 
 
 if __name__ == '__main__':
